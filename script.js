bd25714c859baab585ff734c9147502232b0cefd
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-28 20:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+function generateURL() {
+			var name = document.getElementById("name").value;
+			var year = document.getElementById("year").value;
+
+			var url = "https://localhost:8080/?";
+			if (name != "") {
+				url += "name=" + name;
+				if (year != "") {
+					url += "&year=" + year;
+				}
+			}
+			else if (year != "") {
+				url += "year=" + year;
+			}
+
+			document.getElementById("url").textContent = url;
+		}
